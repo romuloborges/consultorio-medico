8748e961bab5c87a751202bcef5c3967ea09e05a
Rômulo Borges <romuloborges111@gmail.com>
2019-12-23 00:54:13 -0300

    Entidades do domínio criadas

---
+++ ConsultorioMedico-Backend/ConsultorioMedico.Domain/Entity/Medico.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsultorioMedico.Domain.Entity
+{
+    public class Medico
+    {
+        public Guid IdMedico { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public int Crm { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public Guid IdEndereco { get; set; }
+        public Endereco Endereco { get; set; }
+        public List<Agendamento> Agendamentos { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public Medico()
+        {
+
+        }
+
+        public Medico(Guid idMedico, string nome, string cpf, string rg, int crm, DateTime dataNascimento, string sexo, string telefone, string email, Guid idEndereco, Endereco endereco, List<Agendamento> agendamentos, Usuario usuario)
+        {
+            this.IdMedico = idMedico;
+            this.Nome = nome;
+            this.Cpf = cpf;
+            this.Rg = rg;
+            this.Crm = crm;
+            this.DataNascimento = dataNascimento;
+            this.Sexo = sexo;
+            this.Telefone = telefone;
+            this.Email = email;
+            this.IdEndereco = idEndereco;
+            this.Endereco = endereco;
+            this.Agendamentos = agendamentos;
+            this.Usuario = usuario;
+        }
+    }
+}
---
+++ ConsultorioMedico-Backend/ConsultorioMedico.Domain/Entity/Paciente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsultorioMedico.Domain.Entity
+{
+    public class Paciente
+    {
+        public Guid IdPaciente { get; set; }
+        public string Nome { get; set; }
+        public string? NomeSocial { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public string Cpf { get; set; }
+        public string Rg { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public Guid IdEndereco { get; set; }
+        public Endereco Endereco { get; set; }
+        public List<Agendamento> Agendamentos { get; set; }
+
+        public Paciente()
+        {
+
+        }
+
+        public Paciente(Guid idPaciente, string nome, string? nomeSocial, DateTime dataNascimento, string sexo, string cpf, string rg, string telefone, string email, Guid idEndereco, Endereco endereco, List<Agendamento> agendamentos)
+        {
+            this.IdPaciente = idPaciente;
+            this.Nome = nome;
+            this.NomeSocial = nomeSocial;
+            this.DataNascimento = dataNascimento;
+            this.Sexo = sexo;
+            this.Cpf = cpf;
+            this.Rg = rg;
+            this.Telefone = telefone;
+            this.Email = email;
+            this.IdEndereco = idEndereco;
+            this.Endereco = endereco;
+            this.Agendamentos = agendamentos;
+        }
+    }
+}
