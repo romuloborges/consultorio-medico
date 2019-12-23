8be5027700e7fec48ab2ebbfb90ce8cefffaaa44
Rômulo Borges <romuloufes@outlook.com>
2019-12-23 00:54:13 -0300

    Entidades do domínio criadas

---
+++ ConsultorioMedico-Backend/ConsultorioMedico.Domain/Entity/Agendamento.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsultorioMedico.Domain.Entity
+{
+    public class Agendamento
+    {
+        public Guid IdAgendamento { get; set; }
+        public DateTime DataHoraAgendamento { get; set; }
+        public DateTime DataHoraRegistro { get; set; }
+        public Guid IdMedico { get; set; }
+        public Medico Medico { get; set; }
+        public Guid IdPaciente { get; set; }
+        public Paciente Paciente { get; set; }
+        public Consulta? Consulta { get; set; }
+
+        public Agendamento()
+        {
+
+        }
+
+        public Agendamento(Guid idAgendamento, DateTime dataHoraAgendamento, DateTime dataHoraRegistro, Guid idMedico, Medico medico, Guid idPaciente, Paciente paciente, Consulta? consulta)
+        {
+            this.IdAgendamento = idAgendamento;
+            this.DataHoraAgendamento = dataHoraAgendamento;
+            this.DataHoraRegistro = dataHoraRegistro;
+            this.IdMedico = idMedico;
+            this.Medico = medico;
+            this.IdPaciente = idPaciente;
+            this.Paciente = paciente;
+            this.Consulta = consulta;
+        }
+    }
+}
---
+++ ConsultorioMedico-Backend/ConsultorioMedico.Domain/Entity/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsultorioMedico.Domain.Entity
+{
+    public class Usuario
+    {
+        public Guid IdUsuario { get; set; }
+        public string Email { get; set; }
+        public string  Senha { get; set; }
+        public string Tipo { get; set; }
+        public Medico? Medico { get; set; }
+        public Atendente? Atendente { get; set; }
+
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(Guid idUsuario, string email, string senha, string tipo, Medico? medico, Atendente? atendente)
+        {
+            this.IdUsuario = idUsuario;
+            this.Email = email;
+            this.Senha = senha;
+            this.Tipo = tipo;
+            this.Medico = medico;
+            this.Atendente = atendente;
+        }
+    }
+}
