f045e13a4db81e3a4f23681d6521af6b316a75fc
Rômulo Borges <romuloufes@outlook.com>
2019-12-23 14:59:22 -0300

    Finalizado Domain

---
+++ ConsultorioMedico-Backend/ConsultorioMedico.Domain/Repository/IConsultaRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsultorioMedico.Domain.Entity;
+
+namespace ConsultorioMedico.Domain.Repository
+{
+    public interface IConsultaRepository
+    {
+        bool CadastrarConsulta(Consulta consulta);
+        bool AtualizarConsulta(Consulta consulta);
+        IEnumerable<Consulta> ObterTodasConsultas();
+        IEnumerable<Consulta> BuscarConsultaPorData(DateTime dataConsulta);
+        IEnumerable<Consulta> BuscarConsultaPorPaciente(Paciente paciente);
+        bool DeletarConsulta(Consulta consulta);
+    }
+}
---
+++ ConsultorioMedico-Backend/ConsultorioMedico.Domain/Repository/IUsuarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ConsultorioMedico.Domain.Entity;
+
+namespace ConsultorioMedico.Domain.Repository
+{
+    public interface IUsuarioRepository
+    {
+        bool CadastrarUsuario(Usuario usuario);
+        bool AtualizarUsuario(Usuario usuario);
+        Usuario VerificarExistenciaUsuario(string email, string senha);
+        bool DeletarUsuario(Usuario usuario);
+    }
+}
